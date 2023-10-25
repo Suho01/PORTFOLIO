180855f8e58922f210777e60f4880e0279c7c207
이지연 <peaceprimavera@gmail.com>
2023-10-25 17:44:34 +0900

    darkmode실패

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,7 +11,44 @@ function Skills() {
                         <p className='lg:text-8xl md:text-5xl text-3xl'><span className='playfair italic'>S</span>kills</p>
                     </div>
                 </div>
-                
+                <div className='flex lg:justify-between items-center'>
+                    <ul className='lg:basis-1/4 font-bold lg:text-2xl lg:leading-loose'>
+                        <li>Language</li>
+                        <li>Framework / Library</li>
+                        <li>MarkUp</li>
+                        <li>Database</li>
+                        <li>Tool</li>
+                    </ul>
+                    <ul className='lg:basis-3/4 lg:text-lg lg:leading-loose'>
+                        <li className='flex'>
+                            <p>Javascript</p>
+                            <p>Typescript</p>
+                            <p>NextJS</p>
+                        </li>
+                        <li className='flex'>
+                            <p>React</p>
+                            <p>NodeJS</p>
+                        </li>
+                        <li className='flex'>
+                            <p>HTML</p>
+                            <p>CSS</p>
+                            <p>SCSS</p>
+                            <p>TailwindCSS</p>
+                            <p>Styled Components</p>
+                        </li>
+                        <li className='flex'>
+                            <p>MongoDB</p>
+                            <p>MySql</p>
+                        </li>
+                        <li className='flex'>
+                            <p>Git</p>
+                            <p>Github</p>
+                            <p>Figma</p>
+                            <p>Photoshop</p>
+                            <p>Illustrator</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </>
     )
