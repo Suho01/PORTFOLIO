4368d3fadf0c538b47c95632b109f8c194514106
이지연 <peaceprimavera@gmail.com>
2023-11-09 09:35:47 +0900

    footer 여백 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,13 +13,13 @@ function Footer() {
             <p className='lg:text-8xl md:text-5xl text-3xl'><span className='playfair italic'>C</span>onnect Me</p>
         </div>
             <div className='lg:max-w-7xl mx-auto'>
-                <ul className='flex justify-center'>
-                    <li className='pr-10'><NavLink to='https://github.com/Suho01'><FontAwesomeIcon icon={faGithub} className='text-white hover:text-black duration-500 lg:text-9xl md:text-8xl text-7xl' /></NavLink></li>
-                    <li className='pr-10'><NavLink to='https://www.youtube.com/@suhochofree/featured'><FontAwesomeIcon icon={faYoutube} color='white' className='text-white hover:text-red-600 duration-500 lg:text-9xl md:text-8xl text-7xl' /></NavLink></li>
-                    <li className='pr-10'><NavLink to='https://open.kakao.com/me/o2asis'><FontAwesomeIcon icon={faComments} color='white' className='text-white hover:text-yellow-500 duration-500 lg:text-9xl md:text-8xl text-7xl' /></NavLink></li>
+                <ul className='flex justify-around px-[2%]'>
+                    <li><NavLink to='https://github.com/Suho01'><FontAwesomeIcon icon={faGithub} className='text-white hover:text-black duration-500 lg:text-9xl md:text-8xl text-7xl' /></NavLink></li>
+                    <li><NavLink to='https://www.youtube.com/@suhochofree/featured'><FontAwesomeIcon icon={faYoutube} color='white' className='text-white hover:text-red-600 duration-500 lg:text-9xl md:text-8xl text-7xl' /></NavLink></li>
+                    <li><NavLink to='https://open.kakao.com/me/o2asis'><FontAwesomeIcon icon={faComments} color='white' className='text-white hover:text-yellow-500 duration-500 lg:text-9xl md:text-8xl text-7xl' /></NavLink></li>
                     <li><NavLink to="mailto:suhoprimavera@gmail.com"><FontAwesomeIcon icon={faEnvelopeOpen} color='white' className='text-white hover:text-slate-500 duration-500 lg:text-9xl md:text-8xl text-7xl' /></NavLink></li>
                 </ul>
-                <p className='text-lg text-white text-center py-20 tracking-tighter px-5'>Copyright 2023. 이지연 all rights reserved.<br /><br /><span className='lg:text-sm text-xs'>본 사이트는 상업적 목적이 아닌 이지연 개인 포트폴리오 사이트로 제작되었습니다.<br />일부 이미지 및 폰트 등은 그 출처가 따로 있음을 밝힙니다. <br /><br />해당 사이트는 1920*1080 해상도에 최적화 되었습니다.<br /><br />마지막 수정일 : 2023.11.08</span></p>
+                <p className='text-lg text-white text-center py-20 tracking-tighter px-5'>Copyright 2023. 이지연 all rights reserved.<br /><br /><span className='lg:text-sm text-xs'>본 사이트는 상업적 목적이 아닌 이지연 개인 포트폴리오 사이트로 제작되었습니다.<br />일부 이미지 및 폰트 등은 그 출처가 따로 있음을 밝힙니다. <br /><br />해당 사이트는 1920*1080 해상도에 최적화 되었습니다.<br /><br />마지막 수정일 : 2023.11.09</span></p>
             </div>
         </div>
     )
